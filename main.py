12a32926c088391034987676c047c10136486d85
RaviPatel04 <ravidpatel2082004@gmail.com>
2023-08-23 21:29:32 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-import turtle
+# import turtle
 
-turtle.bgcolor("black")
+# turtle.bgcolor("black")
 
-squary = turtle.Turtle()
-squary.speed(15)
-squary.pencolor("red")
-for i in range(400):
-    squary.forward(i)
-    squary.right(1234567890987654321)
+# squary = turtle.Turtle()
+# squary.speed(15)
+# squary.pencolor("red")
+# for i in range(400):
+#     squary.forward(i)
+#     squary.right(1234567890987654321)
 
 
 
+list = [1, 3 , 5]
+sum = 0
+i =0
+
+while i <= len(list):
+    sum += list[i]
+   
+print(sum)
